e16672e3d3216135c4392164a004951f0e3cc1ab
Kishore Narang <narangkishore98@gmail.com>
2023-02-17 16:09:00 -0500

    Initial Commit

---
+++ DotBar/DotBar.h
@@ -0,0 +1,18 @@
+//
+//  DotBar.h
+//  DotBar
+//
+//  Created by Kishore Narang on 2023-02-17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DotBar.
+FOUNDATION_EXPORT double DotBarVersionNumber;
+
+//! Project version string for DotBar.
+FOUNDATION_EXPORT const unsigned char DotBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DotBar/PublicHeader.h>
+
+
